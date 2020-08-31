e5ef45d781c031ee76220c47e488d530394a16cf
Ahmed Nadeem <63425676+ahmednadeem9112@users.noreply.github.com>
2020-09-01 01:13:14 +0500

    Need Some Changes 🏆

---
+++ Public/Profile.js
@@ -0,0 +1,12 @@
+async function ProfileGenerate() {
+const InputField = document.querySelector('input').value;
+const DisplayArea = document.getElementById('outer-box');
+let profile = [];
+
+
+
+
+}
+
+
+
